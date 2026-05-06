66d78bae344d67ba041d56bfc6b6949f9abc69fb
code.neo <61892763+abdullahsmatrix@users.noreply.github.com>
2026-05-07 00:09:06 +0200

    Add algorithm understanding details
    Added algorithm explanation and test case example.

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,4 +1,16 @@
 class Solution:
+    """Understanding the Algorithm: 
+    Test case: [2, 7, 11, 15], Target: 9
+    
+    Lets say we carry a notebook as we enter a room with number 2.
+    We see 2. To hit 9 we need 7. Do we have 7 in notebook?
+    YES: We have the solution.
+    NO: Move to the next room.
+    Since we don't have 7 on our note book, we note burrent room index and number. notebook[room_number] = room_index
+    Next room: 7. To hit 9 we need 2. Do we have 2 in our notebook? Yes. 
+
+    EUREKA! 
+    """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_map: dict = {}
         for i, n in enumerate(nums):
