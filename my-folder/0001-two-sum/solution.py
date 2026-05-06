ce95dd9813e592379356e59c354d70146c67c100
code.neo <61892763+abdullahsmatrix@users.noreply.github.com>
2026-05-06 21:24:41 +0000

    [LeetCode Sync] Runtime - 3 ms (52.20%), Memory - 20.4 MB (43.33%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = 0
-        while i < (len(nums) - 1):
-            j = i+1
-            while j < len(nums):
-                if (nums[i] + nums[j]) == target:
-                    return [i, j]
-                j += 1
-            i += 1 
-        return
+        num_map: dict = {}
+        for i, n in enumerate(nums):
+            difference = target - nums[i]
+            if difference in num_map:
+                return[num_map[difference], i]
+            num_map[n] = i
+        return []
+
